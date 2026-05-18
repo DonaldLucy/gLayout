3a8d7e5517902beebedd774812d20e5c983d8abb
Donald <chuminxing@163.com>
2026-05-18 06:49:25 -0400

    Shrink FVF label pins for DRC margin

--- src/glayout/cells/elementary/FVF/fvf.py
+++ src/glayout/cells/elementary/FVF/fvf.py
@@ -59,20 +59,21 @@ def add_fvf_labels(fvf_in: Component, pdk: MappedPDK) -> Component:
     
     fvf_in.unlock()
     move_info = list()
+    label_pin_size = (0.27, 0.27)
 
-    gnd2label = rectangle(layer=pdk.get_glayer("met1_pin"),size=(0.5,0.5),centered=True).copy()
+    gnd2label = rectangle(layer=pdk.get_glayer("met1_pin"),size=label_pin_size,centered=True).copy()
     gnd2label.add_label(text="VBULK",layer=pdk.get_glayer("met1_label"))
     move_info.append((gnd2label,fvf_in.ports["B_tie_N_top_met_N"],None))
     
-    ibiaslabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.5,0.5),centered=True).copy()
+    ibiaslabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=label_pin_size,centered=True).copy()
     ibiaslabel.add_label(text="Ib",layer=pdk.get_glayer("met2_label"))
     move_info.append((ibiaslabel,fvf_in.ports["A_drain_bottom_met_N"],None))
     
-    outputlabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.5,0.5),centered=True).copy()
+    outputlabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=label_pin_size,centered=True).copy()
     outputlabel.add_label(text="VOUT",layer=pdk.get_glayer("met2_label"))
     move_info.append((outputlabel,fvf_in.ports["A_source_bottom_met_N"],None))
     
-    inputlabel = rectangle(layer=pdk.get_glayer("met1_pin"),size=(0.5,0.5),centered=True).copy()
+    inputlabel = rectangle(layer=pdk.get_glayer("met1_pin"),size=label_pin_size,centered=True).copy()
     inputlabel.add_label(text="VIN",layer=pdk.get_glayer("met1_label"))
     move_info.append((inputlabel,fvf_in.ports["A_multiplier_0_gate_N"], None))
     
